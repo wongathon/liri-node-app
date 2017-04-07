4a27dcb998b1ed3a02f25c844d07d5bbd9b458b7
Andrew Wong <andrewsulliwong@gmail.com>
2017-04-07 16:39:28 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'sXfx0GH1a0q63Z1eH3iLdphvP',
+  consumer_secret: '1WoUJXYUu3ouQovx4AQu5m6tUPHataKzcFHXztCA6kTo763FXg',
+  access_token_key: '2180803478-EiBjsg0lQCEGecMsrgFOVueL5egFQCZ0aTMtmOr',
+  access_token_secret: 'aCvcrKY8pQGF4ElpMbORHOeQzNe7b47ebsjog48XiXTQV',
 }
